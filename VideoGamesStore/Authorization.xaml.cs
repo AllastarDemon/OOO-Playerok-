fda45e87b34f3dfa8d86b8f079b14ecf970e9f34
ArthurPendragon <kostyan.games.2002@gmail.com>
2024-03-17 08:48:07 +0300

    Authorization v1.0.3

--- VideoGamesStore/Authorization.xaml.cs
+++ VideoGamesStore/Authorization.xaml.cs
@@ -1,35 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using VideoGamesStore.Classes;
 using VideoGamesStore.Views;
-using VideoGamesStore.Database;
 
 namespace VideoGamesStore
 {
-    /// <summary>
-    /// Логика взаимодействия для Authorization.xaml
-    /// </summary>
     public partial class Authorization : Window
     {
         Catalog shop;
-        private string name = "Konstantin";
-        private string firstName = "Pendragon";
-
-        private string login = "admin";
-        private string password = "admin";
         public Authorization()
         {
             InitializeComponent();
