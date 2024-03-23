d31098b836db8998e8ee93124eb86e6cf95b0ab8
ArthurPendragon <kostyan.games.2002@gmail.com>
2024-03-23 08:56:26 +0300

    Project Version v0.9.2

--- VideoGamesStore/Authorization.xaml.cs
+++ VideoGamesStore/Authorization.xaml.cs
@@ -2,15 +2,29 @@
 using System.Windows;
 using VideoGamesStore.Classes;
 using VideoGamesStore.Views;
+using EasyCaptcha.Wpf;
+using System;
 
 namespace VideoGamesStore
 {
     public partial class Authorization : Window
     {
         Catalog shop;
+        String captchaText;
         public Authorization()
         {
             InitializeComponent();
+
+            const int lengthCaptcha = 4; 
+            captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, lengthCaptcha);
+            captchaText = captcha.CaptchaText;
+        }
+        private void RegenerateCaptcha(object sender, RoutedEventArgs e)
+        {
+            const int lengthCaptcha = 4;
+            captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, lengthCaptcha);
+            captchaText = captcha.CaptchaText;
+            // Обновляем текст капчи в вашем интерфейсе
         }
         private void OpenCatalogHowGuest(object sender, RoutedEventArgs e)
         {
@@ -30,14 +44,26 @@ namespace VideoGamesStore
                 Helper.user = Helper.DB.User.Where(x => x.UserLogin == LoginTextBox.Text && x.UserPassword == PasswordTextBox.Password).ToList().FirstOrDefault();
                 if (Helper.user != null)
                 {
-                    MessageBox.Show("Вы вошли как: " + Helper.user.UserFullName);
-                    shop = new Catalog(this);
-                    Close();
-                    shop.Show();
+                    if (captchaTextBox.Text == captcha.CaptchaText)
+                    {
+                        MessageBox.Show("Captcha введёна успешно");
+
+                        MessageBox.Show("Вы вошли как: " + Helper.user.UserFullName);
+                        shop = new Catalog(this);
+                        Close();
+                        shop.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Captcha введена не успешно");
+
+                    }
+                    
                 }
                 else
                     MessageBox.Show("Неверный логин или пароль");
             }
+            
         }
     }
 }
